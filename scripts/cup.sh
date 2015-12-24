5786dd2f818a8220090c6c9bf44936a334ec7690
Robert Veznaver <r.veznaver@criteo.com>
2015-12-24 12:53:27 +0100

    add workaround for vagrant-berkshelf bug

--- scripts/cup.sh
+++ scripts/cup.sh
@@ -6,5 +6,9 @@ export LC_CTYPE=en_US.UTF-8
 echo 'Copying files';
 cp /vagrant/cluster/* ./
 
+# workaround for vagrant-berkshelf bug
+echo 'Downloading Vagrant box';
+vagrant box add fgrehm/centos-6-64-lxc
+
 echo 'Starting Vagrant';
 vagrant up
